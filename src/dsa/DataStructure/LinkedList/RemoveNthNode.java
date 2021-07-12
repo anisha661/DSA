ccb867ef158f9bf74e062cb259fcb34acbceeb0b
anisha661 <ahsna06@gmail.com>
2021-07-12 20:52:16 +0545

    Remove nth node from the linked list problem added

---
+++ src/dsa/DataStructure/LinkedList/RemoveNthNode.java
@@ -0,0 +1,27 @@
+package dsa.DataStructure.LinkedList;
+/**
+ * Given the head of a linked list, remove the nth node from the end of the list and return its head.
+ * */
+public class RemoveNthNode {
+
+    public Node removeNthNodeFromTheList(Node head, int n ){
+        Node dummyNode = new Node(0);
+        dummyNode.next = head;
+
+        Node fast = dummyNode;
+        Node slow = dummyNode;
+
+        for(int i = 0;i<=n+1;i++){
+            fast =  fast.next;
+        }
+
+        while(fast != null){
+            slow= slow.next;
+            fast= fast.next;
+        }
+        slow.next = slow.next.next;
+
+        return dummyNode.next;
+
+    }
+}
