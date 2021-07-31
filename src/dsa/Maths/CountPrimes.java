d3da98a2802c2ef91d1edbe3541b9e9994b9c1ef
anisha661 <ahsna06@gmail.com>
2021-07-31 09:23:27 +0545

    Primecount problem added

---
+++ src/dsa/Maths/CountPrimes.java
@@ -0,0 +1,26 @@
+package dsa.Maths;
+/**
+ * Count the number of prime numbers less than a non-negative number, n.
+ * */
+public class CountPrimes {
+
+    public int noOfPrimes(int n){
+        Boolean[] primes = new Boolean[n];
+
+        for(int i =2; i*i<primes.length;i++){
+            if(!primes[i]){
+                for(int j =i;j*i<primes.length;j++){
+                    primes[i*j] = true;
+                }
+            }
+        }
+
+        int primeCount = 0;
+        for(int i=2;i<primes.length;i++){
+            if(!primes[i]){
+                primeCount++;
+            }
+        }
+        return primeCount;
+    }
+}
