247b16227f985566ca9ce0ca45fda6f011881a4f
anisha661 <ahsna06@gmail.com>
2021-08-17 05:57:44 +0545

    Search Insert position problem added

---
+++ src/dsa/DataStructure/Array/InsertPosition.java
@@ -0,0 +1,20 @@
+package dsa.DataStructure.Array;
+
+public class InsertPosition {
+
+    public int searchInsertPosition(int[] nums, int target){
+        int left = 0;
+        int right = nums.length -1;
+        while(left <=right){
+            int mid = left +(right - left)/2;
+            if(target < nums[mid]){
+                right = mid -1;
+            }else if (target > nums[mid]){
+                left = mid +1;
+            }else{
+                return mid;
+            }
+        }
+        return left;
+    }
+}
