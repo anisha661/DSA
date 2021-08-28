f8b2b3202a570d1751c2ca54c1372458761562bd
anisha661 <ahsna06@gmail.com>
2021-08-28 20:02:41 +0545

    LinkedList/Odd even list problem added

---
+++ src/dsa/DataStructure/LinkedList/OddEvenList.java
@@ -0,0 +1,29 @@
+package dsa.DataStructure.LinkedList;
+/**
+ * Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+ *
+ * The first node is considered odd, and the second node is even, and so on.
+ *
+ * Note that the relative order inside both the even and odd groups should remain as it was in the input.
+ * **/
+public class OddEvenList {
+
+    public Node oddEvenList(Node head){
+        if(head == null)
+            return null;
+
+        Node odd = head;
+        Node even = odd.next;
+        Node evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
